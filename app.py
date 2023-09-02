0417b4f43386ad4691fb11532e811bec2c0fd304
shoya1007 <6150fs@gmail.com>
2023-09-02 15:07:54 +0900

    プレート通過時の位置を取得するエンドポイントの追加

--- app.py
+++ app.py
@@ -19,5 +19,15 @@ def get_pitch_movement_data(player_first_name, player_last_name, start_date, end
     return Response(content=buffer.getvalue(), media_type="image/png")
 
 
+@app.get("/pitch_plate")
+def get_pitch_plate(player_first_name, player_last_name, start_date, end_date):
+    collector = DataCollector(player_first_name, player_last_name, start_date, end_date)
+    csv_data = collector.create_pitching_stats_csv()
+    buffer = io.BytesIO()
+    a = Analytics(buffer)
+    a.get_pitch_plate(csv_data)
+    return Response(content=buffer.getvalue(), media_type="image/png")
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
